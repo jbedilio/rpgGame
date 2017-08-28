ed927cf12433e02aa8cf5fd041fae53f3b216e8a
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-28 01:14:47 -0400

    created the background, images, and jq code to set the characters and their attributes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,44 +10,35 @@ var $allChar = [
 var $char = $('#char');
 $.each($allChar, function (index, playa) {
     console.log('button', index, playa);
-    var $character = $('<button><img src="assets/images/' + $allChar[index].playa + '.jpg">')
-    .addClass($allChar[index])
+    var $character = $('<button><img src="assets/images/' + $allChar[index].playa + '.jpg" class="' + $allChar[index].playa + '"><br><p class="'+ $allChar[index].playa+'">'+$allChar[index].hp+'</p>')
+    .addClass('playa ' + $allChar[index].playa)
     .attr('health-points', $allChar[index].hp)
     .attr('attack-points', $allChar[index].ap);
     $character.html(playa[index]);
     $char.append($character);
 });
 
+var $enemies = [
+                { "enemy": "neo", "hp": "180", "ap": "25" },
+                { "enemy": "morphious", "hp": "120", "ap": "8" },
+                { "enemy": "cypher", "hp": "100", "ap": "5" },
+                { "enemy": "mrSmith", "hp": "150", "ap": "25" },
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('.playa').on('click', function () {;
+    var playa = $(this);
+    playa1 = playa;
+    $('#char').find('*').not('.' + playa1).remove();
+    var $enemy = $('#toAttack');
+    $.each($enemies, function (index, playa) {
+        console.log('click', index, playa);
+        var $foe = $('<button><img src="assets/images/' + $enemies[index].enemy + '.jpg" class="' + $allChar[index].playa + '"><br><p class="' + $enemies[index].enemy + '">' + $enemies[index].hp + '</p>')
+        .addClass('enemies ' + $enemies[index].enemy)
+        .attr('health-points', $enemies[index].hp)
+        .attr('attack-points', $enemies[index].ap);
+        $foe.html(playa[index]);
+        $enemy.append($foe); 
+        });
+    });
 
 });
